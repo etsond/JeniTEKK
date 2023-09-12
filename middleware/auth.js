be76cfee3dad436b746ea43a55f212f18de1bac9
Etson <etsond@ufl.edu>
2023-09-12 18:32:24 -0400

    Add additional middleware functions for access control and logging

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,3 +1,21 @@
+// module.exports = {
+//   ensureAuth: function (req, res, next) {
+//     if (req.isAuthenticated()) {
+//       return next();
+//     } else {
+//       res.redirect("/");
+//     }
+//   },
+//   ensureGuest: function (req, res, next) {
+//     if (!req.isAuthenticated()) {
+//       return next();
+//     } else {
+//       res.redirect("/playfeed");
+//     }
+//   },
+// };
+
+
 module.exports = {
   ensureAuth: function (req, res, next) {
     if (req.isAuthenticated()) {
@@ -13,4 +31,21 @@ module.exports = {
       res.redirect("/playfeed");
     }
   },
+  // Additional middleware for special access control
+  ensureAdmin: function (req, res, next) {
+    if (req.user && req.user.isAdmin) {
+      return next(); // User is an admin, proceed
+    } else {
+      res.redirect("/"); // User does not have admin access, redirect to home page
+    }
+  },
+  // Middleware to log requests
+  logRequest: function (req, res, next) {
+    console.log(`Received ${req.method} request for ${req.url}`);
+    next();
+  },
+  // Placeholder function for additional functionality
+  additionalFunction: function (req, res, next) {
+    // Add your custom code here
+  }
 };
