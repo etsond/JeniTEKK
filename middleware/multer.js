ca04fe5fc55aac7a38bd247adabbfeed5b61e581
Etson <etsond@ufl.edu>
2023-09-12 18:30:32 -0400

    Add improved file upload middleware configuration

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,12 +1,29 @@
+// const multer = require("multer");
+// const path = require("path");
+
+// module.exports = multer({
+//   storage: multer.diskStorage({}),
+//   fileFilter: (req, file, cb) => {
+//     let ext = path.extname(file.originalname);
+//     if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png") {
+//       cb(new Error("File type is not supported"), false);
+//       return;
+//     }
+//     cb(null, true);
+//   },
+// });
+
 const multer = require("multer");
 const path = require("path");
+
+const ALLOWED_EXTENSIONS = [".jpg", ".jpeg", ".png"];
 
 module.exports = multer({
   storage: multer.diskStorage({}),
   fileFilter: (req, file, cb) => {
-    let ext = path.extname(file.originalname);
-    if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png") {
-      cb(new Error("File type is not supported"), false);
+    const ext = path.extname(file.originalname);
+    if (!ALLOWED_EXTENSIONS.includes(ext)) {
+      cb(new Error("Only .jpg, .jpeg, and .png files are allowed."), false);
       return;
     }
     cb(null, true);
